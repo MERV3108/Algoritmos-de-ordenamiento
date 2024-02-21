e0e4208719ec8827d0f821619d2718f2817991f9
AleHer1820 <est.giovanny.herna@unimilitar.edu.co>
2024-02-21 16:03:12 -0500

    toy mamao

--- src/algoritmos/de/ordenamiento/Calculadora.java
+++ src/algoritmos/de/ordenamiento/Calculadora.java
@@ -5,18 +5,26 @@ public class Calculadora{
     public int m[][],a[][],b[][],c[][];
     public int fila, columna;
     
-    public void leerMatriz(){
+    public void leerMatrizA(){
     Scanner ja = new Scanner(System.in);
     for (int i=0; i<fila; i++){
         for(int j=0; j<columna; j++){
-            m[i][j] = ja.nextInt();
+            a[i][j] = ja.nextInt();
         }
     }
 }
+     public void leerMatrizB(){
+    Scanner ja = new Scanner(System.in);
+    for (int i=0; i<fila; i++){
+        for(int j=0; j<columna; j++){
+            b[i][j] = ja.nextInt();
+        }
+    }
+    }
     public void imprimirDatos(){
         for(int i=0; i<fila; i++){
             for(int j=0; j<columna; j++){
-               System.out.println(m[i][j]);
+               System.out.println(c[i][j]);
             }
         }
     }
@@ -34,11 +42,28 @@ public class Calculadora{
             }
         }
     }
-    public void multiMatrices(){
+    public void multiplicacion(int filA, int colA, int filB, int colB) {
+  if (colA == filB) {
+    for (int i = 0; i < filA; i++) {
+      for (int j = 0; j < colB; j++) {
+        c[i][j] = a[i][j]*b[j][i];
+        
+      }
+    }
+  } else {
+     System.out.println("No se puede multiplicar matrices que no sean compatibles");
+  }
+}
+
+    public void transpuesta(){
         for(int i=0; i<fila; i++){
             for(int j=0; j<columna; j++){
-                c[i][j]=a[i][j] * b[i][j];
+                b[j][i]=a[i][j];
             }
         }
     }
+    public void determinante2x2(){
+         
+    }
+    
 }
